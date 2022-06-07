a28de329c086903d79af73fb3f781959e6e3222d
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-06-08 00:29:13 +1000

    work12

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -76,20 +76,26 @@ const Post = ({ post, setCurrentId }) => {
         title={post.title}
       />
       <div className={classes.overlay}>
-        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="h6">{post.name}</Typography>
         <Typography variant="body2">
           {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
-      <div className={classes.overlay2}>
-        <Button
-          style={{ color: "white" }}
-          size="small"
-          onClick={() => setCurrentId(post._id)}
-        >
-          <MoreHorizIcon fontSize="medium" />
-        </Button>
-      </div>
+      {(user?.result?.googleId === post?.creator ||
+        user?.result?._id === post?.creator) && (
+        <div className={classes.overlay2} name="edit">
+          <Button
+            onClick={(e) => {
+              e.stopPropagation();
+              setCurrentId(post._id);
+            }}
+            style={{ color: "white" }}
+            size="small"
+          >
+            <MoreHorizIcon fontSize="medium" />
+          </Button>
+        </div>
+      )}
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary" component="h2">
           {post.tags.map((tag) => `#${tag} `)}
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -12,32 +12,53 @@ import {
   FETCH_BY_CREATOR,
 } from "../constants/actionTypes";
 
-export default (state = { isLoading: true, posts: [] }, action) => {
+// export default (state = { isLoading: true, posts: [] }, action) => {
+//   switch (action.type) {
+//     case FETCH_ALL:
+//       return action.payload;
+//     case CREATE:
+//       return { ...state, posts: [...state.posts, action.payload] };
+//     case UPDATE:
+//       return {
+//         ...state,
+//         posts: state.posts.map((post) =>
+//           post._id === action.payload._id ? action.payload : post
+//         ),
+//       };
+//     case DELETE:
+//       return {
+//         ...state,
+//         posts: state.posts.filter((post) => post._id !== action.payload),
+//       };
+//     case LIKE:
+//       return {
+//         ...state,
+//         posts: state.posts.map((post) =>
+//           post._id === action.payload._id ? action.payload : post
+//         ),
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
-    case CREATE:
-      return { ...state, posts: [...state.posts, action.payload] };
-    case UPDATE:
-      return {
-        ...state,
-        posts: state.posts.map((post) =>
-          post._id === action.payload._id ? action.payload : post
-        ),
-      };
-    case DELETE:
-      return {
-        ...state,
-        posts: state.posts.filter((post) => post._id !== action.payload),
-      };
     case LIKE:
-      return {
-        ...state,
-        posts: state.posts.map((post) =>
-          post._id === action.payload._id ? action.payload : post
-        ),
-      };
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case CREATE:
+      return [...posts, action.payload];
+    case UPDATE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case DELETE:
+      return posts.filter((post) => post._id !== action.payload);
     default:
-      return state;
+      return posts;
   }
 };
